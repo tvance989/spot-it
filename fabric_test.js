415de06be7bc2ed89b6f9e26c6f92eb0a1042d85
Taylor Vance <taylorvance@tvMBP14.local>
2015-02-05 23:19:27 -0600

    Random rotate and scale. Problem with coords var scope in img callback....

--- fabric_test.js
+++ fabric_test.js
@@ -4,7 +4,7 @@ var files = [
   'bacon_and_eggs.png',
   'bat.png',
   'beach.jpg',
-  // 'bible.jpg',
+  'bible.jpg',
   'bookshelf.png',
   'bread.png',
   'canoe.jpg',
@@ -22,6 +22,7 @@ var files = [
 var canvas = new fabric.Canvas('canvas');
 
 var radius = 250;
+var imgSize = radius/3;
 
 // Draw card border
 canvas.add(new fabric.Circle({
@@ -39,57 +40,47 @@ coords.push({x:radius, y:radius}); // Start in the middle
 
 var angles = [];
 for (var i = 0; i < 7; i++) {
-  angles.push(i * 2*Math.PI / 7);
+  angles.push(2*Math.PI * i / 7);
 }
-console.log(angles);
 
 for (i in angles) {
-  x = radius + radius*.7 * Math.cos(angles[i]);
-  y = radius + radius*.7 * Math.sin(angles[i]);
+  x = radius + radius*.65 * Math.cos(angles[i]);
+  y = radius + radius*.65 * Math.sin(angles[i]);
   coords.push({x, y});
-  console.log(angles[i], {x,y});
 }
 
+// Draw
 for (i in coords) {
   canvas.add(new fabric.Rect({
     originX:'center',
     originY:'center',
     left: coords[i].x,
     top: coords[i].y,
-    width: 75,
-    height: 75,
+    width: imgSize,
+    height: imgSize,
+    angle: 360*Math.random(),
     fill: '#f33'
-  }));
-  continue;
-
-  fabric.Image.fromURL('img/'+files[1], function(img) {
-    canvas.add(img.set({
-      originX: 'center',
-      originY: 'center',
-      left: coords[i].x,
-      top: coords[i].y,
-      // angle: 45,
-    }).scale(0.15));
-  });
-  console.log(coords[i].x, coords[i].y);
-}
-
-for (i in files) {
-  break;//.del
-  if (i >= 8) {
-    break;
-  }
-
-  var angle = Math.random()*360;
-  console.log(angle);
+  }).scale(Math.random() * .5 + .75));
+  // continue;
 
   fabric.Image.fromURL('img/'+files[i], function(img) {
+    w = img.getWidth();
+    h = img.getHeight();
+    if (w > h) {
+      newWidth = imgSize;
+      newHeight = imgSize - imgSize * (h/w);
+    } else {
+      newWidth = imgSize - imgSize * (w/h);
+      newHeight = imgSize;
+    }
     canvas.add(img.set({
       originX: 'center',
       originY: 'center',
-      top: Math.random()*250+100,
-      left: Math.random()*250+100,
-      angle: angle,
-    }).scale(0.25));
+      left: coords[i].x,//.why doesn't this work?
+      top: coords[i].y,//.why doesn't this work?
+      width: newWidth,
+      height: newHeight,
+      angle: 360*Math.random()
+    }).scale(Math.random() * .5 + .75));
   });
 }
